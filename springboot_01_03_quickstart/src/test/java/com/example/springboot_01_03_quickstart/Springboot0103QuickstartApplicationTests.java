053fd4bddee8f1f4c21422c477aa1cb426064d91
Simpson Song <100260383+fxxtsjs@users.noreply.github.com>
2023-03-08 19:12:12 +0800

    变更

---
+++ springboot_01_03_quickstart/src/test/java/com/example/springboot_01_03_quickstart/Springboot0103QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_01_03_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot0103QuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
