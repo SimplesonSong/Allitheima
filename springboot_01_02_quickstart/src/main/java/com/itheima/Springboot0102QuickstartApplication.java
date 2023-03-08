3c769501202c7803c39ae700d96d7ab46d310ab2
Simpson Song <100260383+fxxtsjs@users.noreply.github.com>
2023-03-08 19:12:34 +0800

    变更

---
+++ springboot_01_02_quickstart/src/main/java/com/itheima/Springboot0102QuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0102QuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot0102QuickstartApplication.class, args);
+	}
+
+}
